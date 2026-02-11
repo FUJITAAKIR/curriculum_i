0d7f3d7c35ed50c5ec33b9106823dfc1fc1ae031
fujita akira <fujita akira@MSI>
2026-02-11 21:10:37 +0900

    curriculum_i

---
+++ curriculum_i/src/curriculum_i/Main.java
@@ -0,0 +1,21 @@
+package curriculum_i;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	public static void main(String []args) {
+		List<Employee>employeeList=new ArrayList<>();
+		employeeList.add(new FullTimeEmployee("F001","田中"));
+		employeeList.add(new ContractEmployee("C001","佐藤"));
+		employeeList.add(new FullTimeEmployee("F002","鈴木"));
+		employeeList.add(new ContractEmployee("C002","高橋"));
+		
+		int hoursWorked=10;
+		for (Employee emp:employeeList) {
+			int wage=emp.calculateDailyWage(hoursWorked);
+			System.out.println(emp.name+"の日給は"+wage+"円です。");
+		}
+	}
+
+}
---
+++ curriculum_i/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module curriculum_i {
+}
